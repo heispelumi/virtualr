846682b0f4bfbf79a09e1c72616af813472f8485
heispelumi <pelumiatoyebi06@gmail.com>
2025-01-01 21:08:52 +0100

    commit

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import {pricing} from './MainData'
 
 const Pricing = () => {
   return (
-    <div>
-          <h1>hey</h1>
+    <div className='relative md:max-w-7xl mx-auto h-full overflow-x-hidden lg:px-6 overflow-y-hidden border-b border-neutral-800' >
+     <h2 className="text-3xl sm:text-5xl lg:text-6xl text-center my-8 tracking-wide">
+        Pricing
+      </h2>
     </div>
   )
 }
